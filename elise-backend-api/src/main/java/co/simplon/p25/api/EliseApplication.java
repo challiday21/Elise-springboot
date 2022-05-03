a8cf7f99425d99284ba92b987253877d3b604033
Claire HALLIDAY <clairehalliday73@gmail.com>
2022-05-03 13:05:01 +0200

    first commit

---
+++ elise-backend-api/src/main/java/co/simplon/p25/api/EliseApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.p25.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliseApplication.class, args);
+	}
+
+}
